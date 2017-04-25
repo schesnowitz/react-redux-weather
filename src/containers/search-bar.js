29c5d8559b3f3cd9cd923678b29e8181092f98e6
schesnowitz <steve@chesnowitz.com>
2017-04-25 01:59:31 -0400

    input callback with bind

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''}; 
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event){
+    console.warn(event.target.value);
+    this.setState({term: event.target.value});
+  }  
+
   render() {
     return(
       <form className='input-group'>
-      <input />
+      <input 
+      placeholder="Search a City"
+      className="form-control"
+      value={this.state.term}
+      onChange={this.onInputChange}
+      />
       <span className='input-group-btn'>
       <button type="submit" className="btn btn-secondary">Search</button>
       </span>

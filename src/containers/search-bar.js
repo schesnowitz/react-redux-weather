248aad77307f7121021a235c75ff0f25af9d8668
schesnowitz <steve@chesnowitz.com>
2017-04-25 02:06:07 -0400

    prevent default on submit avoid reaload on press of enter of click

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -15,9 +15,13 @@ export class SearchBar extends Component {
     this.setState({term: event.target.value});
   }  
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return(
-      <form className='input-group'>
+      <form onSubmit={this.onFormSubmit} className='input-group'>
       <input 
       placeholder="Search a City"
       className="form-control"

888be6dd0818ac348e7be3befe6091ef22d5618e
schesnowitz <steve@chesnowitz.com>
2017-04-26 01:42:31 -0400

    adding g maps

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -8,29 +8,35 @@ class WeatherList extends Component {
 
   renderWeather(cityData) {
     const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const celsius = _.map(cityData.list.map(weather => weather.main.temp), (celsius) => celsius - 273.15);
+    const fahrenheit = _.map(cityData.list.map(weather => weather.main.temp), (fahrenheit) => fahrenheit * 9/5 - 459.67);    
+    // console.log(temps);
     return(
 		<tr key={cityData.city.name}>
     <th>{cityData.city.name}</th>
-    <th>
-    <Chart data={temps} color='blue'/>
-    </th>
+    <th><Chart data={celsius} color='red' units="C"/></th>
+    <th><Chart data={fahrenheit} color='red' units="F"/></th>
+    <th><Chart data={pressure} color='blue' units="hPa"/></th>
+    <th><Chart data={humidity} color='green' units="%"/></th>
 		</tr>
     );
   }
   render() {
     return(
-<table className="table table-hover table-condensed table-responsive">
+<table className="table table-hover">
 	<thead>
 		<tr>
 			<th>City</th>
-			<th>Temperature</th>
+			<th>celsius</th>
+    	<th>°F</th>
 			<th>Pressure</th>
 			<th>Humidity</th>
 		</tr>
 	</thead>
 	<tbody>
-{this.props.weather.map(this.renderWeather)}
+  {this.props.weather.map(this.renderWeather)}
 	</tbody>
 </table>
     );
